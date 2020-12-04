a35460dbbaf456c6aaed40b7a90296aaafb666e5
Adrian <adrian17091996@gmail.com>
2020-12-04 16:19:30 +0100

    first commit

---
+++ src/main/java/Transfer.java
@@ -0,0 +1,11 @@
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class Transfer {
+    private String senderAccount;
+    private String recipientAccount;
+    private String title;
+    private BigDecimal amount;
+}
